02059d1219bdde49e16067bd296cc174100b3050
Alejandro Mery <amery@jpi.io>
2024-01-28 23:21:44 +0000

    config: extend NewPathError() to reuse the original *os.PathError when it exists
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- config/errors.go
+++ config/errors.go
@@ -1,9 +1,23 @@
 package config
 
-import "os"
+import (
+	"os"
+
+	"darvaza.org/core"
+)
 
 // NewPathError is a shortcut to generate [os.PathError].
 func NewPathError(filename, op string, err error) *os.PathError {
+	if e, ok := err.(*os.PathError); ok {
+		// unwrap and reuse information if possible.
+		return &os.PathError{
+			Path: core.Coalesce(e.Path, filename),
+			Op:   core.Coalesce(e.Op, op),
+			Err:  e.Err,
+		}
+	}
+
+	// wrap
 	return &os.PathError{
 		Path: filename,
 		Op:   op,
