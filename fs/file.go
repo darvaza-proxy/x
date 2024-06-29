22a823882e87c4a35a794d958184fb1d0da46fbc
Alejandro Mery <amery@jpi.io>
2024-06-29 22:42:51 +0000

    fs: add fs.File and fs.ReadDirFile aliases
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ fs/file.go
@@ -0,0 +1,9 @@
+package fs
+
+import "io/fs"
+
+// File is an alias of the standard [fs.File] interface.
+type File = fs.File
+
+// ReadDirFile is an alias of the standard [fs.ReadDirFile] interface.
+type ReadDirFile = fs.ReadDirFile
