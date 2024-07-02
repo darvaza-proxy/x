7da8e4fba3ad7133420635f86c1b8f8e016b8191
Alejandro Mery <amery@jpi.io>
2024-07-02 15:32:50 +0000

    web/assets: introduce ContentType() and ContentTyped() interface
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/assets/http.go
+++ web/assets/http.go
@@ -1,10 +1,43 @@
 package assets
 
 import (
+	"io/fs"
 	"net/http"
 
 	"darvaza.org/x/web"
 )
+
+// ContentTyped allows files to declare their Content-Type.
+type ContentTyped interface {
+	ContentType() string
+}
+
+// ContentType checks if the [fs.File] or its [fs.FileInfo] effectively
+// provides the Content-Type, and returns it if so.
+func ContentType(file fs.File) string {
+	if ct := tryContentType(file); ct != "" {
+		return ct
+	}
+
+	fi, _ := file.Stat()
+	if fi != nil {
+		return tryContentType(fi)
+	}
+
+	return ""
+}
+
+func tryContentType(candidates ...any) string {
+	for _, x := range candidates {
+		if v, ok := x.(ContentTyped); ok {
+			if ct := v.ContentType(); ct != "" {
+				return ct
+			}
+		}
+	}
+
+	return ""
+}
 
 // DefaultRequestResolver returns the requested path
 func DefaultRequestResolver(req *http.Request) (string, error) {
