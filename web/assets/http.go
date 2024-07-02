b019a270a6cbc87b5d6982f51abb43db774e025c
Alejandro Mery <amery@jpi.io>
2024-07-02 15:47:42 +0000

    web/assets: introduce ETag() and ETaged interface
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/assets/http.go
+++ web/assets/http.go
@@ -39,6 +39,38 @@ func tryContentType(candidates ...any) string {
 	return ""
 }
 
+// ETaged allows files to declare their ETag to enable
+// proper caching.
+type ETaged interface {
+	ETag() []string
+}
+
+// ETag checks the [fs.File] and [fs.FileInfo] to see
+// if they implement the [ETaged] interface, and return
+// their output.
+func ETag(file fs.File) []string {
+	if s := tryETag(file); len(s) > 0 {
+		return s
+	}
+
+	if fi, _ := file.Stat(); fi != nil {
+		return tryETag(fi)
+	}
+
+	return nil
+}
+
+func tryETag(candidates ...any) []string {
+	for _, x := range candidates {
+		if v, ok := x.(ETaged); ok {
+			if s := v.ETag(); len(s) > 0 {
+				return s
+			}
+		}
+	}
+	return nil
+}
+
 // DefaultRequestResolver returns the requested path
 func DefaultRequestResolver(req *http.Request) (string, error) {
 	var path string
