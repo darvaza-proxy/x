3e268261dac3457904454155dd65e31751dafbcf
Alejandro Mery <amery@jpi.io>
2024-07-04 18:17:47 +0000

    web: HandleError: a default error handler
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/error_handler.go
+++ web/error_handler.go
@@ -24,3 +24,35 @@ func ErrorHandler(ctx context.Context) (ErrorHandlerFunc, bool) {
 var (
 	errCtxKey = core.NewContextKey[ErrorHandlerFunc]("ErrorHandler")
 )
+
+// HandleError attempts to call the [ErrorHandler] set for the content,
+// otherwise it serves the error directly using its own ServeHTTP if defined
+// or [HTTPError] if not.
+func HandleError(rw http.ResponseWriter, req *http.Request, err error) {
+	var h http.Handler
+
+	if fn, _ := ErrorHandler(req.Context()); fn != nil {
+		// pass over to the error handler
+		fn(rw, req, err)
+		return
+	}
+
+	if e, ok := err.(http.Handler); ok {
+		// direct
+		h = e
+	} else {
+		// assemble
+		var code int
+
+		if e, ok := err.(Error); ok {
+			code = e.Status()
+		}
+
+		h = &HTTPError{
+			Code: core.IIf(code > 0, code, http.StatusInternalServerError),
+			Err:  err,
+		}
+	}
+
+	h.ServeHTTP(rw, req)
+}
