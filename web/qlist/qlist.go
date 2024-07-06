e1eb5b160ab0d7eb697e7bb2b72b157615400d38
Alejandro Mery <amery@jpi.io>
2024-07-06 22:05:06 +0000

    web/qlist: introduce QualityValue.IsMediaType() checker
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/qlist/qlist.go
+++ web/qlist/qlist.go
@@ -75,6 +75,26 @@ func (q QualityValue) String() string {
 	return strings.Join(s, ";")
 }
 
+// IsMediaType checks if the [QualityValue] represents
+// only specific Media Type and not a Media Range.
+func (q QualityValue) IsMediaType() bool {
+	return len(q.value) == 2 &&
+		q.quality > 0 &&
+		isMediaTypePart(q.value[0]) &&
+		isMediaTypePart(q.value[1])
+}
+
+func isMediaTypePart(s string) bool {
+	switch {
+	case s == "":
+		return false
+	case strings.Contains(s, "*"):
+		return false
+	default:
+		return strings.ToLower(s) == s
+	}
+}
+
 // Match answers the question if we match a target.
 // For an entry to match another it needs to have the
 // same number of parts, and each pair of parts be
