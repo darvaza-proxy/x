0549a5a6962688c4ba8971c3b240e93f9695f347
Alejandro Mery <amery@jpi.io>
2024-07-07 20:09:02 +0000

    web/forms: ParseForm with basic JSON support
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/forms/forms.go
+++ web/forms/forms.go
@@ -1,2 +1,103 @@
 // Package forms assists working with web forms and Request.Form
 package forms
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"darvaza.org/x/web"
+)
+
+// DefaultFormMaxMemory indicates the memory limit when parsing a form
+// used when ParseForm is called without a positive number.
+const DefaultFormMaxMemory = 1 << 20 // 1MiB
+
+// ParseForm is similar to the standard request.ParseForm() but it
+// handles urlencoded, multipart and JSON.
+// For nested JSON objects ParseForm uses dots to join keys.
+func ParseForm(req *http.Request, maxMemory int64) (err error) {
+	if req.Form != nil {
+		// ready
+		return nil
+	}
+
+	ct := getContentType(req)
+	switch ct {
+	case "application/x-www-form-urlencoded":
+		err = req.ParseForm()
+	case "multipart/form-data":
+		err = req.ParseMultipartForm(maxMemory)
+	case "application/json":
+		err = parseFormJSON(req, maxMemory)
+	default:
+		return &web.HTTPError{Code: http.StatusUnsupportedMediaType}
+	}
+
+	return web.AsErrorWithCode(err, http.StatusBadRequest)
+}
+
+// ReadAll read the whole body of a request but fails if it exceeds
+// the given limit.
+// If no limit is provided, DefaultFormMaxMemory will be used.
+func ReadAll(body io.Reader, maxBytes int64) ([]byte, error) {
+	if maxBytes <= 0 {
+		maxBytes = DefaultFormMaxMemory
+	}
+
+	r := &io.LimitedReader{
+		R: body,
+		N: maxBytes + 1,
+	}
+
+	b, err := io.ReadAll(r)
+	switch {
+	case err != nil:
+		return b, err
+	case int64(len(b)) > maxBytes:
+		return nil, errors.New("size limit exceeded")
+	default:
+		return b, nil
+	}
+}
+
+func parseFormJSON(req *http.Request, maxMemory int64) error {
+	b, err := ReadAll(req.Body, maxMemory)
+	if err != nil {
+		return err
+	}
+
+	values, err := UnmarshalFormJSON(b)
+	if err != nil {
+		return err
+	}
+
+	switch strings.ToUpper(req.Method) {
+	case "POST", "PUT", "PATCH":
+		req.PostForm = cloneValues(values)
+	default:
+		req.PostForm = make(url.Values)
+	}
+
+	// TODO: add query string values
+	req.Form = values
+
+	return nil
+}
+
+func cloneValues(orig url.Values) url.Values {
+	out := make(url.Values)
+	for k, s := range orig {
+		s2 := make([]string, len(s))
+		copy(s2, s)
+		out[k] = s2
+	}
+	return out
+}
+
+func getContentType(req *http.Request) string {
+	s := strings.Split(req.Header.Get("Content-Type"), ";")[0]
+	return strings.ToLower(strings.TrimSpace(s))
+}
---
+++ web/forms/json.go
@@ -0,0 +1,55 @@
+package forms
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"darvaza.org/core"
+)
+
+// UnmarshalFormJSON attempts to convert a JSON document
+// into a url.Values map.
+func UnmarshalFormJSON(b []byte) (url.Values, error) {
+	// parse
+	data := make(map[string]any)
+	if err := json.Unmarshal(b, &data); err != nil {
+		return nil, err
+	}
+
+	// flatten
+	values := make(url.Values)
+	for k, v := range data {
+		jsonValue(values, k, v)
+	}
+
+	return values, nil
+}
+
+func jsonValue(out url.Values, key string, v any) {
+	switch s := v.(type) {
+	case nil:
+		// Null
+		if _, ok := out[key]; !ok {
+			out[key] = []string{}
+		}
+	case string:
+		// String
+		out.Add(key, s)
+	case []any:
+		// Array
+		for _, v := range s {
+			jsonValue(out, key, v)
+		}
+	case map[string]any:
+		// Object
+		for k, v := range s {
+			jsonValue(out, key+"."+k, v)
+		}
+	case bool:
+		// Boolean]
+		out.Add(key, core.IIf(s, "true", "false"))
+	default: // Number
+		out.Add(key, fmt.Sprint(v))
+	}
+}
