2501ba1594945983f2658443734a939d4c18982c
Alejandro Mery <amery@jpi.io>
2024-07-08 17:45:33 +0000

    net/reconnect: introduce Config{} and use it to create the Client{}
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- net/reconnect/client.go
+++ net/reconnect/client.go
@@ -15,25 +15,48 @@ type Client struct {
 	ctx    context.Context
 	cancel context.CancelCauseFunc
 	err    error
+
+	cfg *Config
 }
 
-// New creates a new [Client].
-func New(ctx context.Context) (*Client, error) {
-	if ctx == nil {
-		ctx = context.Background()
+// Config returns the [Config] object used when [Reload] is called.
+func (c *Client) Config() *Config {
+	return c.cfg
+}
+
+// Reload attempts to apply changes done to the [Config] since the
+// last time, or since created.
+func (c *Client) Reload() error {
+	if err := c.cfg.Valid(); err != nil {
+		return err
 	}
 
-	ctx, cancel := context.WithCancelCause(ctx)
+	return core.ErrTODO
+}
+
+// New creates a new [Client] using the given [Config] and options
+func New(cfg *Config, options ...OptionFunc) (*Client, error) {
+	cfg, err := prepareNewConfig(cfg, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancelCause(cfg.Context)
+
 	c := &Client{
 		ctx:    ctx,
 		cancel: cancel,
+
+		cfg: cfg,
 	}
+
+	cfg.unsafeBindClient(c)
 	return c, nil
 }
 
 // Must is like [New] but it panics on errors.
-func Must(ctx context.Context) *Client {
-	c, err := New(ctx)
+func Must(cfg *Config, options ...OptionFunc) *Client {
+	c, err := New(cfg, options...)
 	if err != nil {
 		core.Panic(err)
 	}
---
+++ net/reconnect/config.go
@@ -0,0 +1,101 @@
+package reconnect
+
+import (
+	"context"
+	"errors"
+	"io/fs"
+
+	"darvaza.org/core"
+)
+
+// A OptionFunc modifies a [Config] consistently before SetDefaults() and Validate().
+type OptionFunc func(*Config) error
+
+var (
+	// ErrConfigBusy indicates the [Config] is in used and can't
+	// be used to create another [Client].
+	ErrConfigBusy = core.QuietWrap(fs.ErrPermission, "config already in use")
+)
+
+// Config describes the operation of the Client.
+type Config struct {
+	Context context.Context
+
+	// immutable data
+	c   *Client
+	ctx context.Context
+}
+
+func (cfg *Config) unsafeBindClient(c *Client) {
+	// set immutables
+	cfg.c = c
+	cfg.ctx = cfg.Context
+}
+
+// busy indicates the immutables have already been set.
+func (cfg *Config) busy() bool {
+	return cfg.c != nil
+}
+
+// SetDefaults fills any gap in the config
+func (cfg *Config) SetDefaults() error {
+	if cfg.Context == nil {
+		// either the immutable context or a fresh one
+		cfg.Context = core.Coalesce(cfg.ctx, context.Background())
+	}
+	return nil
+}
+
+// Valid checks if the [Config] is fit to be used.
+func (cfg *Config) Valid() error {
+	// basic rules
+	switch {
+	case cfg.Context == nil:
+		return errors.New("context missing")
+	default:
+		// TODO: more rules
+	}
+
+	if cfg.busy() {
+		// bound rules
+		return cfg.validateBusy()
+	}
+
+	return nil
+}
+
+func (cfg *Config) validateBusy() error {
+	switch {
+	case cfg.Context != cfg.ctx:
+		return errors.New("invalid context")
+	default:
+		// TODO: more rules
+	}
+
+	return nil
+}
+
+func prepareNewConfig(cfg *Config, options ...OptionFunc) (*Config, error) {
+	switch {
+	case cfg == nil:
+		cfg = new(Config)
+	case cfg.busy():
+		return nil, ErrConfigBusy
+	}
+
+	for _, fn := range options {
+		if err := fn(cfg); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := cfg.SetDefaults(); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.Valid(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
