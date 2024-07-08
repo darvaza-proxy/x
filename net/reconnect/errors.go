05c3f909f9db40cd25ef23e9384d5da1acf9b03c
Alejandro Mery <amery@jpi.io>
2024-07-08 16:36:29 +0000

    net/reconnect: ErrAbnormalConnect and ErrNotConnected
    ErrAbnormalConnect is an annotated ECONNABORTED
    ErrNotConnected is an annotated ErrClosed
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- net/reconnect/errors.go
+++ net/reconnect/errors.go
@@ -1,9 +1,22 @@
 package reconnect
 
-import "errors"
+import (
+	"errors"
+	"io/fs"
+	"syscall"
+
+	"darvaza.org/core"
+)
 
 var (
+	// ErrAbnormalConnect indicates the dialer didn't return error
+	// nor connection.
+	ErrAbnormalConnect = core.QuietWrap(syscall.ECONNABORTED, "abnormal response")
+
 	// ErrDoNotReconnect indicates the Waiter
 	// instructed us to not reconnect
 	ErrDoNotReconnect = errors.New("don't reconnect")
+
+	// ErrNotConnected indicates the [Client] isn't currently connected.
+	ErrNotConnected = core.QuietWrap(fs.ErrClosed, "connection closed")
 )
