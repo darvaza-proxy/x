d38979081902159221f8355f509df9da96a41f61
Alejandro Mery <amery@jpi.io>
2024-07-08 16:44:41 +0000

    net/reconnect: IsFatal() determines if the reconnection loop should be aborted
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- net/reconnect/errors.go
+++ net/reconnect/errors.go
@@ -20,3 +20,43 @@ var (
 	// ErrNotConnected indicates the [Client] isn't currently connected.
 	ErrNotConnected = core.QuietWrap(fs.ErrClosed, "connection closed")
 )
+
+// IsFatal tells if the error means the connection
+// should be closed and not retried.
+func IsFatal(err error) bool {
+	if err != nil {
+		is, _ := core.IsErrorFn2(checkIsFatal, err)
+		return is
+	}
+	return false
+}
+
+func checkIsFatal(err error) (is, certainly bool) {
+	switch err {
+	case ErrDoNotReconnect:
+		// do-not-reconnect
+		return true, true
+	default:
+		// normal errors are never fatal
+		if is, _ := checkIsExpectable(err); is {
+			return false, true
+		}
+
+		// reconnect if temporary
+		return core.CheckIsTemporary(err)
+	}
+}
+
+func checkIsExpectable(err error) (is, certainly bool) {
+	switch err {
+	case fs.ErrClosed,
+		syscall.ECONNABORTED,
+		syscall.ECONNREFUSED,
+		syscall.ECONNRESET:
+		// reconnect
+		return true, true
+	default:
+		// unknown
+		return false, false
+	}
+}
