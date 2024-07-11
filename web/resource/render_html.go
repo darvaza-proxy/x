6a6d3618f7d6e96bb98245b3e17eb51ebd4cb2bb
Alejandro Mery <amery@jpi.io>
2024-07-11 21:28:40 +0000

    web/resource: introduce RenderHTMLWithCode() to be able to send non-200 responses
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/resource/render_html.go
+++ web/resource/render_html.go
@@ -19,17 +19,36 @@ func htmlRendererOf[T any](x any) (HandlerFunc[T], bool) {
 // RenderHTML compiles an html/template and sends it to the client after setting
 // Content-Type and Content-Length.  For HEAD only Content-Type is set.
 func RenderHTML(rw http.ResponseWriter, req *http.Request, tmpl *template.Template, data any) error {
+	return RenderHTMLWithCode(rw, req, 0, tmpl, data)
+}
+
+// RenderHTMLWithCode compiles an html/template and sends it to the client after setting
+// Content-Type and Content-Length with a given HTTP status code.
+// For HEAD only Content-Type is set.
+func RenderHTMLWithCode(rw http.ResponseWriter, req *http.Request, code int, tmpl *template.Template, data any) error {
 	SetHeader(rw, ContentType, HTML)
+
+	switch {
+	case code < 0:
+		code = http.StatusInternalServerError
+	case code == 0:
+		code = http.StatusOK
+	}
 
 	if req.Method == "HEAD" {
 		// done
+		if code == http.StatusOK {
+			code = http.StatusNoContent
+		}
+
+		rw.WriteHeader(code)
 		return nil
 	}
 
-	return doRenderHTML(rw, tmpl, data)
+	return doRenderHTML(rw, tmpl, code, data)
 }
 
-func doRenderHTML(rw http.ResponseWriter, tmpl *template.Template, data any) error {
+func doRenderHTML(rw http.ResponseWriter, tmpl *template.Template, code int, data any) error {
 	var buf bytes.Buffer
 
 	if err := tmpl.Execute(&buf, data); err != nil {
@@ -37,6 +56,8 @@ func doRenderHTML(rw http.ResponseWriter, tmpl *template.Template, data any) err
 	}
 
 	SetHeader(rw, ContentLength, "%v", buf.Len())
+	rw.WriteHeader(code)
+
 	_, err := buf.WriteTo(rw)
 	return err
 }
