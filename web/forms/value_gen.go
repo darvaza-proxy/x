0a87f7ecb02a4c4d7b50d167a4d5e53950eac6a6
Alejandro Mery <amery@jpi.io>
2024-07-15 17:08:41 +0000

    web/forms: introduce FormValuesFoo and PostFormValuesFoo
    like FormValueFoo and PostFormValueFoo but for all values
    of the fields instead of just the first.
    
    they will stop at the first conversion error, but return
    all successfully converted values to that point.
    
    * FormValuesSigned
    * FormValuesUnsigned
    * FormValuesFloat
    * FormValuesBool
    * PostFormValuesSigned
    * PostFormValuesUnsigned
    * PostFormValuesFloat
    * PostFormValuesBool
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/forms/value_gen.go
+++ web/forms/value_gen.go
@@ -27,6 +27,28 @@ func FormValueSigned[T core.Signed](req *http.Request, field string) (value T, f
 	return value, found, err
 }
 
+// FormValuesSigned reads a field from [http.Request#Form], after populating it if needed,
+// and returns its [core.Signed] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func FormValuesSigned[T core.Signed](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := FormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseSigned[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
+}
+
 // FormValueUnsigned reads a field from [http.Request#Form], after populating it if needed,
 // and returns a [core.Unsigned] value, an indicator saying if it was actually present
 // and possibly an error.
@@ -42,6 +64,28 @@ func FormValueUnsigned[T core.Unsigned](req *http.Request, field string) (value 
 	}
 
 	return value, found, err
+}
+
+// FormValuesUnsigned reads a field from [http.Request#Form], after populating it if needed,
+// and returns its [core.Unsigned] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func FormValuesUnsigned[T core.Unsigned](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := FormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseUnsigned[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
 }
 
 // FormValueFloat reads a field from [http.Request#Form], after populating it if needed,
@@ -61,6 +105,28 @@ func FormValueFloat[T core.Float](req *http.Request, field string) (value T, fou
 	return value, found, err
 }
 
+// FormValuesFloat reads a field from [http.Request#Form], after populating it if needed,
+// and returns its [core.Float] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func FormValuesFloat[T core.Float](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := FormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseFloat[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
+}
+
 // FormValueBool reads a field from [http.Request#Form], after populating it if needed,
 // and returns a [core.Bool] value, an indicator saying if it was actually present
 // and possibly an error.
@@ -76,6 +142,28 @@ func FormValueBool[T core.Bool](req *http.Request, field string) (value T, found
 	}
 
 	return value, found, err
+}
+
+// FormValuesBool reads a field from [http.Request#Form], after populating it if needed,
+// and returns its [core.Bool] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func FormValuesBool[T core.Bool](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := FormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseBool[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
 }
 
 // PostFormValueSigned reads a field from [http.Request#PostForm], after populating it if needed,
@@ -95,6 +183,28 @@ func PostFormValueSigned[T core.Signed](req *http.Request, field string) (value 
 	return value, found, err
 }
 
+// PostFormValuesSigned reads a field from [http.Request#PostForm], after populating it if needed,
+// and returns its [core.Signed] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func PostFormValuesSigned[T core.Signed](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := PostFormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseSigned[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
+}
+
 // PostFormValueUnsigned reads a field from [http.Request#PostForm], after populating it if needed,
 // and returns a [core.Unsigned] value, an indicator saying if it was actually present
 // and possibly an error.
@@ -110,6 +220,28 @@ func PostFormValueUnsigned[T core.Unsigned](req *http.Request, field string) (va
 	}
 
 	return value, found, err
+}
+
+// PostFormValuesUnsigned reads a field from [http.Request#PostForm], after populating it if needed,
+// and returns its [core.Unsigned] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func PostFormValuesUnsigned[T core.Unsigned](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := PostFormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseUnsigned[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
 }
 
 // PostFormValueFloat reads a field from [http.Request#PostForm], after populating it if needed,
@@ -129,6 +261,28 @@ func PostFormValueFloat[T core.Float](req *http.Request, field string) (value T,
 	return value, found, err
 }
 
+// PostFormValuesFloat reads a field from [http.Request#PostForm], after populating it if needed,
+// and returns its [core.Float] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func PostFormValuesFloat[T core.Float](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := PostFormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseFloat[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
+}
+
 // PostFormValueBool reads a field from [http.Request#PostForm], after populating it if needed,
 // and returns a [core.Bool] value, an indicator saying if it was actually present
 // and possibly an error.
@@ -144,4 +298,26 @@ func PostFormValueBool[T core.Bool](req *http.Request, field string) (value T, f
 	}
 
 	return value, found, err
+}
+
+// PostFormValuesBool reads a field from [http.Request#PostForm], after populating it if needed,
+// and returns its [core.Bool] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func PostFormValuesBool[T core.Bool](req *http.Request, field string) (values []T, found bool, err error) {
+	ss, found, err := PostFormValues[string](req, field)
+	if err == nil && found {
+		values = make([]T, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ParseBool[T](s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
 }
--- web/forms/value_gen.sh
+++ web/forms/value_gen.sh
@@ -31,12 +31,16 @@ to_lower() {
 gen() {
 	local x="$1" y="$2" g="$3"
 	local fn fn0 fn1
+	local fn0s fn1s
 	local parse
 	local T ret
 
 	fn="Parse${y}"
 	fn0="${x}Value"
+	fn0s="${x}Values"
+
 	fn1="${fn0}${g}"
+	fn1s="${fn0s}${g}"
 
 	parse="Parse$g"
 	T=true
@@ -59,6 +63,28 @@ func ${fn1}${T:+[T core.$g]}(req *http.Request, field string) (value ${ret}, fou
 	}
 
 	return value, found, err
+}
+
+// $fn1s reads a field from [http.Request#$x], after populating it if needed,
+// and returns its [core.$g] values, an indicator saying if it was actually present
+// and possibly an error.
+// Errors could indicate [ParseForm] failed, or a [strconv.NumError] if it
+// couldn't be converted to the intended type.
+func ${fn1s}${T:+[T core.$g]}(req *http.Request, field string) (values []${ret}, found bool, err error) {
+	ss, found, err := ${fn0s}[string](req, field)
+	if err == nil && found {
+		values = make([]$ret, 0, len(ss))
+
+		for _, s := range ss {
+			v, err := ${parse}${T:+[T]}(s)
+			if err != nil {
+				return values, true, core.Wrap(err, field)
+			}
+
+			values = append(values, v)
+		}
+	}
+	return values, found, err
 }
 EOT
 }
