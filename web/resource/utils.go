fe60db4b1809a0c68c2dd6976d26c4a0a8b63369
Alejandro Mery <amery@jpi.io>
2024-07-16 16:57:17 +0000

    web/resource: introduce SetHeaderUnlessExists()
    to set a fallback header value without overriding what the
    user might have set already
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/resource/utils.go
+++ web/resource/utils.go
@@ -7,9 +7,30 @@ import (
 
 // SetHeader sets a header value, optionally formatted.
 func SetHeader(rw http.ResponseWriter, key, value string, args ...any) {
+	doSetHeader(rw.Header(), key, value, args...)
+}
+
+// SetHeaderUnlessExists sets a header value if it's not already set.
+// Optional value formatting supported.
+func SetHeaderUnlessExists(rw http.ResponseWriter, key, value string, args ...any) {
+	hdr := rw.Header()
+	if !headerExists(hdr, key) {
+		doSetHeader(hdr, key, value, args...)
+	}
+}
+
+func doSetHeader(hdr http.Header, key, value string, args ...any) {
 	if len(args) > 0 {
 		value = fmt.Sprintf(value, args...)
 	}
 
-	rw.Header().Set(key, value)
+	hdr.Set(key, value)
+}
+
+func headerExists(hdr http.Header, key string) bool {
+	s, ok := hdr[key]
+	if !ok || len(s) == 0 || s[0] == "" {
+		return false
+	}
+	return true
 }
