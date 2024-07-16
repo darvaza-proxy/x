58f600d2c2fc281651b2bc2cfdc3e8ad1e8d8695
Alejandro Mery <amery@jpi.io>
2024-07-16 16:58:37 +0000

    web/resource: introduce SetCache()
    supporting "private", "no-cache" and "max-age" values
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/resource/utils.go
+++ web/resource/utils.go
@@ -3,6 +3,9 @@ package resource
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"darvaza.org/x/web/consts"
 )
 
 // SetHeader sets a header value, optionally formatted.
@@ -33,4 +36,24 @@ func headerExists(hdr http.Header, key string) bool {
 		return false
 	}
 	return true
+}
+
+// SetCache sets the Cache-Control header. a Negative
+// value is translated in "private", values of a second
+// or longer translated to "max-age", and otherwise
+// "no-cache"
+func SetCache(rw http.ResponseWriter, d time.Duration) {
+	rw.Header().Set(consts.CacheControl, cacheControlValue(d))
+}
+
+func cacheControlValue(d time.Duration) string {
+	if d < 0 {
+		return "private"
+	}
+
+	if sec := d / time.Second; sec > 0 {
+		return fmt.Sprintf("max-age=%v", sec)
+	}
+
+	return "no-cache"
 }
