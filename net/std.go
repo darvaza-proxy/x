8c2cb6662ee699a2a8c40438fea8089d10a5cce6
Alejandro Mery <amery@jpi.io>
2024-07-20 14:56:32 +0000

    net: add aliases for TCPAddr, TCPConn and TCPListener
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- net/std.go
+++ net/std.go
@@ -7,10 +7,16 @@ import (
 type (
 	// Addr is an alias of the standard net.Addr type
 	Addr = net.Addr
+	// TCPAddr is an alias of the standard net.TCPAddr type
+	TCPAddr = net.TCPAddr
 	// Conn is an alias of the standard net.Conn type
 	Conn = net.Conn
-	// IP is an alias of the stadnard net.IP type
+	// TCPConn is an alias of the standard net.TCPConn type
+	TCPConn = net.TCPConn
+	// IP is an alias of the standard net.IP type
 	IP = net.IP
 	// Listener is an alias of the standard net.Listener type
 	Listener = net.Listener
+	// TCPListener is an alias of the standard net.TCPListener type
+	TCPListener = net.TCPListener
 )
