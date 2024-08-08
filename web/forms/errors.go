4cc72199d910a69adee8a94dfc9f9ea78faf0489
Alejandro Mery <amery@jpi.io>
2024-08-08 15:27:29 +0000

    web/forms: IsEmptyString() and IsNilOrEmptyString()
    tests errors to see if the field was empty.
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- web/forms/errors.go
+++ web/forms/errors.go
@@ -24,3 +24,28 @@ func errRange(fn, s string) error {
 		Err:  strconv.ErrRange,
 	}
 }
+
+// IsEmptyString tells if the given error was caused by the argument
+// being an empty string.
+func IsEmptyString(err error) bool {
+	switch e := err.(type) {
+	case *strconv.NumError:
+		return e.Err == strconv.ErrSyntax && e.Num == ""
+	default:
+		return false
+	}
+}
+
+// IsNilOrEmptyString tells if the given error was caused by the argument
+// being an empty string but it also succeeds when no error is given.
+// To be used when testing an error condition.
+func IsNilOrEmptyString(err error) bool {
+	switch e := err.(type) {
+	case nil:
+		return true
+	case *strconv.NumError:
+		return e.Err == strconv.ErrSyntax && e.Num == ""
+	default:
+		return false
+	}
+}
