958c9e74378f49458bafe0ce6e3e998180db9d93
Alejandro Mery <amery@apptly.co>
2024-09-24 13:27:02 +0000

    fs/fssyscall: introduce LockEx() and UnlockEx()
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- fs/fssyscall/syscall_linux.go
+++ fs/fssyscall/syscall_linux.go
@@ -2,6 +2,10 @@
 
 package fssyscall
 
+import (
+	"syscall"
+)
+
 // ZeroHandle represents a closed [Handle]
 const ZeroHandle Handle = -1
 
@@ -10,3 +14,14 @@ type Handle int
 
 // Sys returns the underlying type for syscall operations
 func (h Handle) Sys() int { return int(h) }
+
+// LockEx attempts to create an advisory exclusive lock on
+// the file associated to the given [Handle].
+func LockEx(h Handle) error {
+	return syscall.Flock(h.Sys(), syscall.LOCK_EX)
+}
+
+// UnlockEx releases an advisory lock on the file associated with the given Handle.
+func UnlockEx(h Handle) error {
+	return syscall.Flock(h.Sys(), syscall.LOCK_UN)
+}
--- fs/fssyscall/syscall_windows.go
+++ fs/fssyscall/syscall_windows.go
@@ -14,3 +14,21 @@ type Handle syscall.Handle
 
 // Sys returns the underlying type for syscall operations
 func (h Handle) Sys() syscall.Handle { return syscall.Handle(h) }
+
+// LockEx is a no-op function that simulates an advisory exclusive lock on the file
+// associated to the given [Handle].
+//
+// Note: This function does not provide actual locking functionality on Windows.
+// It exists for API compatibility with other platforms.
+//
+// TODO: implement
+func LockEx(Handle) error { return nil }
+
+// UnlockEx is a no-op function that releases an advisory lock on the file associated
+// with the given Handle.
+//
+// Note: This function does not provide actual locking functionality on Windows.
+// It exists for API compatibility with other platforms.
+//
+// TODO: implement
+func UnlockEx(Handle) error { return nil }
