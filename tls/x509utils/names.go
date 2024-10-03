66469f8306e34bcd899120f0701323a1c9f9d4ae
Alejandro Mery <amery@apptly.co>
2024-10-03 01:52:50 +0000

    tls/x509utils: add SanitizeName()
    to prepare a hostname to be matched against a certificate.
    
    imported from darvaza.org/darvaza/shared/x509utils
    
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- tls/x509utils/names.go
+++ tls/x509utils/names.go
@@ -50,3 +50,38 @@ func appendIPAddresses(names []string, addrs []net.IP) []string {
 	}
 	return names
 }
+
+// SanitizeName takes a Hostname and returns the name (or address)
+// we will use for matching certificates
+func SanitizeName(name string) (string, bool) {
+	if name != "" {
+		// validate and remove port and brackets if present
+		if host, _, err := core.SplitHostPort(name); err == nil {
+			return doSanitizeName(host)
+		}
+	}
+	return "", false
+}
+
+func doSanitizeName(name string) (string, bool) {
+	if addr, err := core.ParseAddr(name); err == nil {
+		// IP
+		addr = addr.Unmap()
+		addr = addr.WithZone("")
+		name = addr.String()
+	} else {
+		// Name
+		name = removeZone(name)
+	}
+	return name, len(name) > 0
+}
+
+func removeZone(name string) string {
+	idx := strings.LastIndexFunc(name, func(r rune) bool {
+		return r == '%'
+	})
+	if idx < 0 {
+		return name
+	}
+	return name[:idx]
+}
