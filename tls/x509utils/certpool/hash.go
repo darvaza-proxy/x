295f86f60fc8512f8f44d9eafb15e26db0a0637e
Alejandro Mery <amery@apptly.co>
2024-10-04 19:39:19 +0000

    tls/x509utils/certpool: introduce BLAKE3 based Hash for certificates
    Signed-off-by: Alejandro Mery <amery@apptly.co>

---
+++ tls/x509utils/certpool/hash.go
@@ -0,0 +1,44 @@
+package certpool
+
+import (
+	"crypto/x509"
+
+	"github.com/zeebo/blake3"
+)
+
+const (
+	// HashSize is the number of bytes of HashCert's output
+	HashSize = 32
+)
+
+// Hash is a blake3.Sum256 representation of a DER encoded certificate
+type Hash [HashSize]byte
+
+// Equal says if a hash is identical to this one.
+func (hash Hash) Equal(other Hash) bool {
+	return hash == other
+}
+
+// EqualCert says if the certificate matches the hash.
+func (hash Hash) EqualCert(cert *x509.Certificate) bool {
+	other, ok := HashCert(cert)
+	if !ok {
+		return false
+	}
+
+	return hash.Equal(other)
+}
+
+// IsZero tells if the hash is at its zero value.
+func (hash Hash) IsZero() bool {
+	return hash == Hash{}
+}
+
+// HashCert produces a blake3 digest of the DER representation of a Certificate
+func HashCert(cert *x509.Certificate) (Hash, bool) {
+	if cert == nil || len(cert.Raw) == 0 {
+		return Hash{}, false
+	}
+
+	return blake3.Sum256(cert.Raw), true
+}
