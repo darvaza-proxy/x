85acb4c5cec55ac90f1e86c07b6306535629f13a
Alejandro Mery <amery@apptly.co>
2024-10-13 22:26:50 +0000

    tls/x509utils: add additional comparison helpers
    * PrivateKeyEqual()
    * PublicKeyEqual()
    * ValidKeyPair()
    
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- tls/x509utils/pair.go
+++ tls/x509utils/pair.go
@@ -1,18 +1,50 @@
 package x509utils
 
-import "crypto/x509"
+import (
+	"crypto"
+	"crypto/x509"
+)
+
+// PrivateKeyEqual tells if two private keys are the same.
+// nil keys aren't considered comparable.
+func PrivateKeyEqual(a, b crypto.PrivateKey) bool {
+	if a2, ok := a.(PrivateKey); ok {
+		return a2.Equal(b)
+	}
+	return false
+}
+
+// PublicKeyEqual tells if two public keys are the same.
+// nil keys aren't considered comparable.
+func PublicKeyEqual(a, b crypto.PublicKey) bool {
+	if a2, ok := a.(PublicKey); ok {
+		return a2.Equal(b)
+	}
+	return false
+}
 
 // ValidCertKeyPair confirms the given key can use the given certificate.
-func ValidCertKeyPair(cert *x509.Certificate, key PrivateKey) bool {
+// nil keys aren't considered comparable.
+func ValidCertKeyPair(cert *x509.Certificate, key crypto.PrivateKey) bool {
 	if cert == nil || key == nil {
-		// missing
-		return false
-	}
-	pub, ok := cert.PublicKey.(PublicKey)
-	if !ok {
-		// invalid - unreachable
 		return false
 	}
 
-	return pub.Equal(key.Public())
+	if pk, ok := key.(PrivateKey); ok {
+		return PublicKeyEqual(cert.PublicKey, pk.Public())
+	}
+
+	return false
+}
+
+// ValidKeyPair confirms the public key matches the private one.
+// nil keys aren't considered comparable.
+func ValidKeyPair(pub crypto.PublicKey, key crypto.PrivateKey) bool {
+	if pub == nil || key == nil {
+		return false
+	}
+	if priv, ok := key.(PrivateKey); ok {
+		return PublicKeyEqual(pub, priv.Public())
+	}
+	return false
 }
