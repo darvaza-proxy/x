ee766c4f296f454b2ffb7aa78485b42cebbdedab
Alejandro Mery <amery@apptly.co>
2024-10-13 21:00:57 +0000

    tls/x509utils: introduce IsSelfSigned() checker
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- tls/x509utils/pair.go
+++ tls/x509utils/pair.go
@@ -1,6 +1,7 @@
 package x509utils
 
 import (
+	"bytes"
 	"crypto"
 	"crypto/x509"
 )
@@ -45,6 +46,21 @@ func ValidKeyPair(pub crypto.PublicKey, key crypto.PrivateKey) bool {
 	}
 	if priv, ok := key.(PrivateKey); ok {
 		return PublicKeyEqual(pub, priv.Public())
+	}
+	return false
+}
+
+// IsSelfSigned tests if a certificate corresponds to a self-signed CA.
+func IsSelfSigned(c *x509.Certificate) bool {
+	if c != nil && c.IsCA && bytes.Equal(c.RawSubject, c.RawIssuer) {
+		pool := x509.NewCertPool()
+
+		pool.AddCert(c)
+		_, err := c.Verify(x509.VerifyOptions{
+			Roots: pool,
+		})
+
+		return err == nil
 	}
 	return false
 }
