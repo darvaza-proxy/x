9a4fb87f03f2e0012062c0e3196ef157258c11f0
Alejandro Mery <amery@apptly.co>
2024-10-21 18:26:50 +0000

    tls: improve time checks in Verify() when called without roots
    Signed-off-by: Alejandro Mery <amery@apptly.co>

--- tls/verify.go
+++ tls/verify.go
@@ -13,6 +13,8 @@ import (
 // Verify checks if a [tls.Certificate] is good to use.
 // If roots is provided, the chain will also be verified.
 func Verify(cert *tls.Certificate, roots *x509.CertPool) error {
+	now := time.Now().UTC()
+
 	switch {
 	case cert == nil:
 		return &x509utils.ErrInvalidCert{
@@ -23,8 +25,8 @@ func Verify(cert *tls.Certificate, roots *x509.CertPool) error {
 			Reason: "missing leaf certificate",
 		}
 	case len(cert.Leaf.Raw) == 0,
-		cert.Leaf.NotAfter.Before(time.Now()),
-		cert.Leaf.NotBefore.After(time.Now()),
+		cert.Leaf.NotAfter.Before(now),
+		cert.Leaf.NotBefore.After(now),
 		!bytes.Equal(cert.Leaf.Raw, cert.Certificate[0]):
 		return &x509utils.ErrInvalidCert{
 			Reason: "invalid leaf certificate",
@@ -38,19 +40,20 @@ func Verify(cert *tls.Certificate, roots *x509.CertPool) error {
 			Reason: "invalid private key",
 		}
 	case roots != nil:
-		return doVerifyRoots(cert, roots)
+		return doVerifyRoots(cert, roots, now)
 	default:
-		return doVerifyNoRoots(cert)
+		return doVerifyNoRoots(cert, now)
 	}
 }
 
-func doVerifyRoots(cert *tls.Certificate, roots *x509.CertPool) error {
+func doVerifyRoots(cert *tls.Certificate, roots *x509.CertPool, now time.Time) error {
 	opt := x509.VerifyOptions{
 		Roots:         roots,
 		Intermediates: x509.NewCertPool(),
+		CurrentTime:   now,
 	}
 	for i, der := range cert.Certificate[1:] {
-		c, err := doVerifyDERCert(i+1, der)
+		c, err := doVerifyDERCert(i+1, der, nil)
 		if err != nil {
 			return core.Wrap(err, "ReadCertificate")
 		}
@@ -58,12 +61,15 @@ func doVerifyRoots(cert *tls.Certificate, roots *x509.CertPool) error {
 		opt.Intermediates.AddCert(c)
 	}
 	_, err := cert.Leaf.Verify(opt)
-	return err
+	if err != nil {
+		return core.Wrap(err, "failed to validate certificates chain")
+	}
+	return nil
 }
 
-func doVerifyNoRoots(cert *tls.Certificate) error {
+func doVerifyNoRoots(cert *tls.Certificate, now time.Time) error {
 	for i, der := range cert.Certificate {
-		_, err := doVerifyDERCert(i, der)
+		_, err := doVerifyDERCert(i, der, &now)
 		if err != nil {
 			return core.Wrap(err, "ReadCertificate")
 		}
@@ -71,10 +77,17 @@ func doVerifyNoRoots(cert *tls.Certificate) error {
 	return nil
 }
 
-func doVerifyDERCert(slot int, der []byte) (*x509.Certificate, error) {
+func doVerifyDERCert(slot int, der []byte, now *time.Time) (*x509.Certificate, error) {
 	cert, err := x509.ParseCertificate(der)
 	if err != nil {
 		return nil, core.Wrapf(err, "bad certificate in slot %v", slot)
 	}
+
+	if now != nil {
+		if now.After(cert.NotAfter) || now.Before(cert.NotBefore) {
+			return nil, core.Wrapf(core.ErrInvalid, "expired certificate in slot %v", slot)
+		}
+	}
+
 	return cert, nil
 }
